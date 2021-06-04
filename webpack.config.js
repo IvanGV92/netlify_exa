b777ad1d81d517712b5a8c6c16051dd0e4e38710
IvanGV92 <ianlee9204@gmail.com>
2021-06-04 15:01:09 -0500

    [add] first commit

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  //Por donde ingresa a la aplicacion
+  entry: './src/index.js',
+  //en donde se va a crear los distribuibles y con que nombre
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  //Que extensiones va a manejar el proyecto. Si se manejan frameworks se tienen que agregar aca
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname,'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/')
+    }
+  },
+  module:{
+    //Se establecen las reglas que debe cumplir como la prueba con las extensions por expresion regular o el loader que es babel
+    rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.css|.styl$/i,
+      use: [MiniCssExtractPlugin.loader,
+      'css-loader',
+      'stylus-loader'
+    ],
+
+    },
+    {
+      test: /\.png/,
+      type: 'asset/resource'
+    },
+    {
+      test:/\.(woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: "application/font-woff",
+          name: "[name].[contenthash].[ext]",
+          outputPath: "./assets/fonts/",
+          publicPath: "../assets/fonts/",
+          esModule:false,
+        }
+      }
+    }
+  ],
+
+},
+   plugins: [
+    //Configuracion para optimizar y desplegar HTML
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname,"src","assets/images"),
+          to: "assets/images"
+        }
+      ]
+    }),
+    new Dotenv(),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ]
+  }
+}
